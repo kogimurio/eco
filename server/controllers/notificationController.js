7cb3826f7807f1be22ee6ee6a043e5a2a5297245
Kenneth <kogimurio@gmail.com>
2025-08-18 17:04:22 +0300

    ADDED: MarkAsRead notification function

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -24,3 +24,32 @@ exports.getNotifications = async (req, res) => {
         });
     }
 }
+
+exports.markAsRead = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const notification = await Notification.findByIdAndUpdate(
+            id,
+            { read: true },
+            { new: true }
+        );
+        if (!notification) {
+            return res.status(404).json({
+                success: false,
+                message: "Notification not found"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "notification marked as read",
+            notification
+        })
+    } catch (error) {
+        console.error("Error marking notification as read:", error);
+        res.status(500).json({
+            success: false,
+            error: "Internal server error"
+        })
+    }
+};
+
--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth')
 
 const {
-    getNotifications
+    getNotifications,
+    markAsRead
 } = require('../controllers/notificationController');
 
 router.get('/',
     authMiddleware,
     getNotifications
+)
+
+router.patch('/:id/read',
+    authMiddleware,
+    markAsRead
 )
 
 module.exports = router;
