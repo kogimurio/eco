38fdfbb219e44ae45cbe47277e2bac671915e211
Kenneth <kogimurio@gmail.com>
2025-09-05 22:14:55 +0300

    UPDATED: SortAndPagination

--- client/src/pages/CategoryComponents/CategoryDetail.jsx
+++ client/src/pages/CategoryComponents/CategoryDetail.jsx
@@ -15,6 +15,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export default function CategoryDetail() {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const [selectedFilters, setSelectedFilters] = useState({
     brand: [],
     size: [],
@@ -29,7 +30,14 @@ export default function CategoryDetail() {
   const { addToCart } = useCart();
   const token = JSON.parse(localStorage.getItem("token"));
 
-
+  // Fetch all related products
+  useEffect(() => {
+    const fetchAll = async() => {
+      const response = await axios.get(`${BASE_URL}/category/${slug}`);
+      setAllProducts(response.data.products);
+    };
+    fetchAll();
+  }, [slug]);
   // Fetched filtered products
   useEffect(() => {
     const fetchProduct = async () => {
@@ -119,12 +127,16 @@ export default function CategoryDetail() {
               <FilterSidebar
                 selectedFilters={selectedFilters}
                 setSelectedFilters={setSelectedFilters}
-                products={products} // pass for counts
+                products={allProducts} // pass for counts
               />
 
               {/* Main Content */}
               <div className="grid grid-cols-1">
-                <SortAndPagination />
+                <SortAndPagination 
+                  selectedFilters={selectedFilters}
+                  setSelectedFilters={setSelectedFilters}
+                />
+
                 <div className="bg-gray-800 py-4 overflow-x-hidden">
                   <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full mx-auto">
                     {products.map((product, index) => (
